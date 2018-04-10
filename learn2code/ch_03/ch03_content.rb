997bc7486f0a150c705bd92ed77771f1148e3242
Andy Holmes <andyavd01@gmail.com>
2018-04-09 20:59:55 -0400

    initial

---
+++ learn2code/ch_03/ch03_content.rb
@@ -0,0 +1,32 @@
+# Following are examples given in the chapter 3 text
+
+puts "Hello, world"
+puts ''
+puts 'Good-bye.'
+
+# string "arithmetic"
+
+puts 'I like' + 'apple pie.'
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+
+puts 'blink ' * 4
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+puts "These are some examples that cause errors:"
+puts "puts 'Betty' + 12"
+puts "puts 'Fred' * ' John'"
+
+# Escape character \
+
+puts 'You\'re swell!'
+puts 'Backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
