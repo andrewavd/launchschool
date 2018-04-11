a5f6342f59361a5f3744ed3eed470c182ddcb20d
Andy Holmes <andyavd01@gmail.com>
2018-04-11 10:36:24 -0400

    refactor while

--- learn2code/ch_07/ch07_exercises.rb
+++ learn2code/ch_07/ch07_exercises.rb
@@ -2,7 +2,8 @@
 puts "How many bottles do you wish to start with?"
 bottles = gets.chomp.to_i
 
-while bottles > 0
+#while bottles > 0
+bottles.times do
   puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer,"
   puts "take one down, pass it around, #{bottles - 1} bottles of beer on the wall!"
   puts
