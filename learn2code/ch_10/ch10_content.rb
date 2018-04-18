056f3453fa232823ad9d56b8a53d3fbfdf7fd859
Andy Holmes <andyavd01@gmail.com>
2018-04-18 12:12:14 -0400

    add english word recursive version

--- learn2code/ch_10/ch10_content.rb
+++ learn2code/ch_10/ch10_content.rb
@@ -137,9 +137,10 @@ puts unsorted == [] ? "There is nothing to sort!": "This is the sorted list: #{S
 
 # English words
 # nightmare version
+=begin
 def english_number(number)
   if number < 0
-    return "Please enter a numver zero or greater."
+    return "Please enter a number zero or greater."
   end
   if number > 100
     return "Please enter a number 100 or less."
@@ -241,3 +242,69 @@ puts english_number(32)
 puts english_number(88)
 puts english_number(99)
 puts english_number(100)
+=end
+
+# English number recursive version
+
+def english_number(number)
+  if number < 0
+    return "Please enter a number zero or greater."
+  end
+  if number == 0
+    return "zero"
+  end
+
+  num_string = ""
+  ones_place = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  tens_place = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+  teenagers = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+  
+  left = number
+  write = left/100
+  left = left - write*100
+
+  if write > 0
+    hundreds = english_number(write)
+    num_string = num_string + hundreds + " hundred"
+    if left > 0
+      num_string = num_string + " "
+    end
+  end
+  write = left/10
+  left = left - write*10
+
+  if write > 0
+    if ((write == 1) && (left >0))
+      num_string = num_string + teenagers[left-1]
+      left = 0
+    else
+      num_string = num_string + tens_place[write-1]
+    end
+    if left > 0
+      num_string = num_string + "-"
+    end
+  end
+
+  write = left
+  left = 0
+
+  if write > 0
+    num_string = num_string + ones_place[write-1]
+  end
+  num_string
+end
+
+puts english_number(0)
+puts english_number(9)
+puts english_number(10)
+puts english_number(11)
+puts english_number(17)
+puts english_number(32)
+puts english_number(88)
+puts english_number(99)
+puts english_number(100)
+puts english_number(101)
+puts english_number(234)
+puts english_number(3211)
+puts english_number(999999)
+puts english_number(1000000000000)
