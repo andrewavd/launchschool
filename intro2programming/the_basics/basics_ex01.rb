5e2493feaa9508a44baf693690537537eaf8ead3
Andy Holmes <andyavd01@gmail.com>
2018-04-19 11:14:58 -0400

    add comments

--- intro2programming/the_basics/basics_ex01.rb
+++ intro2programming/the_basics/basics_ex01.rb
@@ -1,4 +1,8 @@
+# Add two strings together that, when concatenated, return your first and last name as your full name in one string.
+
 puts 'Andy' + ' ' + 'Holmes'
+
+# Not concatenation, but aother way to do it
 first_name = 'Andy'
 last_name = 'Holmes'
 puts "#{first_name} #{last_name}"
