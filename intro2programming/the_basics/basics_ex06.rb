7e81a1befd72d7e1c9d629b90d088f6ad9472e41
Andy Holmes <andyavd01@gmail.com>
2018-04-19 12:48:14 -0400

    initial

---
+++ intro2programming/the_basics/basics_ex06.rb
@@ -0,0 +1,5 @@
+# Write a program that calculates the squares of 3 float numbers
+# of your choosing and outputs the result to the screen.
+
+nums  = [1.0, 2.5, 7.25]
+nums.each { |x| puts "The square of #{x} is #{x*x}."}
