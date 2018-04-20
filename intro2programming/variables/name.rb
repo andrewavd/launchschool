a8fa3d497ade3ee69950cfb35774547b679a028f
Andy Holmes <andyavd01@gmail.com>
2018-04-19 21:12:15 -0400

    accept input of first and last names

--- intro2programming/variables/name.rb
+++ intro2programming/variables/name.rb
@@ -1,12 +1,18 @@
-# Write a program called name.rb that asks the user to type in their
+# (Exercise #1) Write a program called name.rb that asks the user to type in their
 # name and then prints out a greeting message with their name included.
 
-# Add another section onto name.rb that prints the name of the user 10 times.
+# (Exercise #3) Add another section onto name.rb that prints the name of the user 10 times.
 # You must do this without explicitly writing the puts method 10 times in a row.
-# Hint: you can use the times method to do something repeatedly.#
+# Hint: you can use the times method to do something repeatedly.
 
-print "Hi, what's your name? "
-name = gets.chomp
+# (Exercise #4) Modify name.rb again so that it first asks the user for their first name,
+# saves it into a variable, and then does the same for the last name.
+# Then outputs their full name all at once.
+
+print "Hi, what's your first name? "
+first_name = gets.chomp
+print "What's your last name? "
+last_name = gets.chomp
 10.times do 
-  puts "Hi #{name}, nice to meet you!"
+  puts "Hi #{first_name} #{last_name}, nice to meet you!"
 end
