ebaeb28bd5f0e3ae9a52ec74196764f0e9923808
Andy Holmes <andyavd01@gmail.com>
2018-04-19 20:54:10 -0400

    initial

---
+++ intro2programming/variables/name.rb
@@ -0,0 +1,6 @@
+# Write a program called name.rb that asks the user to type in their
+# name and then prints out a greeting message with their name included.
+
+print "Please enter your name: "
+name = gets.chomp
+puts "Hi #{name}, nice to meet you!"
