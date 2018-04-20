b533efcb20ccfefefb04c441cf62e0016c757515
Andy Holmes <andyavd01@gmail.com>
2018-04-19 21:07:34 -0400

    output name multiple times

--- intro2programming/variables/name.rb
+++ intro2programming/variables/name.rb
@@ -1,6 +1,12 @@
 # Write a program called name.rb that asks the user to type in their
 # name and then prints out a greeting message with their name included.
 
-print "Please enter your name: "
+# Add another section onto name.rb that prints the name of the user 10 times.
+# You must do this without explicitly writing the puts method 10 times in a row.
+# Hint: you can use the times method to do something repeatedly.#
+
+print "Hi, what's your name? "
 name = gets.chomp
-puts "Hi #{name}, nice to meet you!"
+10.times do 
+  puts "Hi #{name}, nice to meet you!"
+end
