7421b9e732f6a633f1ada087705b95e0a76b4fb2
Andy Holmes <andyavd01@gmail.com>
2018-04-20 20:22:52 -0400

    initial

---
+++ intro2programming/flow_control/flowcontrol_ex06.rb
@@ -0,0 +1,13 @@
+# When you run the following code...
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+  end # Original code had no closing end for the if statement.
+end
+
+equal_to_four(5)
+
+# When you run the following code...
+# exercise.rb:8: syntax error, unexpected end-of-input, expecting keyword_end
