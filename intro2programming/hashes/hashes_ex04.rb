9572bdc8cca715a24e4d4537f683eb20fa87ebcb
Andy Holmes <andyavd01@gmail.com>
2018-04-24 13:45:32 -0400

    initial

---
+++ intro2programming/hashes/hashes_ex04.rb
@@ -0,0 +1,9 @@
+# Given the following expression, how would you access the name of the person?
+
+person = {
+  name: "Andy",
+  occupation: "wev developer",
+  hobbies: "running"
+}
+
+puts person[:name]
