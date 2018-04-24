2e18465f4f8200ca95eedd0a3ce0cb9b16904a9e
Andy Holmes <andyavd01@gmail.com>
2018-04-24 19:58:40 -0400

    initial

---
+++ intro2programming/hashes/hashes_ex07.rb
@@ -0,0 +1,9 @@
+# Given the following code...
+
+x = "hi there"
+
+my_hash = {x: "some value"}
+my_hash2 = {x => "some value"}
+
+# What's the difference between the two hashes that were created?
+# Answer: my_hash uses a symbol as a key, my_hash2 uses a string as a key.
