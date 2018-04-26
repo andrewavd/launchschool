c7e75209c872fb4703a10d104e9d12310537fa3c
Andy Holmes <andyavd01@gmail.com>
2018-04-26 09:37:16 -0400

    initial

---
+++ intro2programming/exercises/ex08.rb
@@ -0,0 +1,8 @@
+# April 26, 2018
+# Create a Hash using both Ruby syntax styles.
+
+old_hash = {:day => 'Sunday'}
+new_hash = {day: 'Monday'}
+
+puts "This is the old style hash: #{old_hash}"
+p new_hash
