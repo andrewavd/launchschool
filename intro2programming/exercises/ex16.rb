5f84a313b8a254cb3af8c8d56e8dcdb9f0e54a24
Andy Holmes <andyavd01@gmail.com>
2018-05-15 18:23:07 -0400

    format

--- intro2programming/exercises/ex16.rb
+++ intro2programming/exercises/ex16.rb
@@ -4,6 +4,6 @@
 
 a = ['white snow', 'winter wonderland', 'melting ice', 'slippery sidewalk', 'salted roads', 'white trees']
 
-a.map! { |x| x.split(' ')}.flatten!
+a.map! { |x| x.split(' ') }.flatten!
 
 p a
