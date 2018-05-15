5a6a1de03611f5134af785c894a8987bb9ef4d5d
Andy Holmes <andyavd01@gmail.com>
2018-05-15 16:58:20 -0400

    initial

---
+++ intro2programming/exercises/ex16.rb
@@ -0,0 +1,10 @@
+# May 15, 2018
+# Take the given array and turn it into a new array that consosts of strings containing
+# one word.
+
+a = ['white snow', 'winter wonderland', 'melting ice', 'slippery sidewalk', 'salted roads', 'white trees']
+
+a.map! { |x| x.split(' ')}.flatten!
+
+
+p a
