1f1aa9e4f28a5be8d7682640207add94d5574bff
Andy Holmes <andyavd01@gmail.com>
2018-07-11 08:52:59 -0400

    initial

---
+++ exercises/101-109_small_problems/how_many.rb
@@ -0,0 +1,12 @@
+# Write a method that counts the number of occurrences of each element in a given array.
+
+def count_occurrences(vehicles)
+  unique = vehicles.uniq
+  unique.each do |vehicle|
+    puts("#{vehicle} => #{vehicles.count(vehicle)}")
+  end
+end
+
+vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck', 'motorcycle', 'motorcycle', 'car', 'truck']
+
+count_occurrences(vehicles)
