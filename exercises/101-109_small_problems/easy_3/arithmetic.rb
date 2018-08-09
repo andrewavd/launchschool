6e2af83494cfca5704de60bf0fedf55647cb20e8
Andy Holmes <andyavd01@gmail.com>
2018-08-09 09:43:54 -0400

    add extras

--- exercises/101-109_small_problems/easy_3/arithmetic.rb
+++ exercises/101-109_small_problems/easy_3/arithmetic.rb
@@ -5,18 +5,30 @@
 # addition, subtraction, product, quotient and power. Do not worry about
 # validating the input.
 
+# Discussion
+# There are some edge cases to consider in this exercise. We have to be careful
+# of not having a second number that is zero. (for division anyway) Waht if we
+# wanted to use floats instead of integers? How does this change the problem?
+
 def prompt(msg)
   print "==> #{msg}"
 end
 
 prompt "Enter the first number: "
-num1 = gets.chomp.to_i
+# num1 = gets.chomp.to_i
+num1 = gets.chomp.to_f
 prompt "Enter the second number: "
-num2 = gets.chomp.to_i
+# num2 = gets.chomp.to_i
+num2 = gets.chomp.to_f
 
 puts "#{num1} + #{num2} = #{num1 + num2}"
 puts "#{num1} - #{num2} = #{num1 - num2}"
 puts "#{num1} * #{num2} = #{num1 * num2}"
-puts "#{num1} / #{num2} = #{num1 / num2}"
+# original puts "#{num1} / #{num2} = #{num1 / num2}"
+if num2 == 0
+  puts "Sorry... You can't divide by zero!"
+else
+  puts "#{num1} / #{num2} = #{num1 / num2}"
+end
 puts "#{num1} % #{num2} = #{num1 % num2}"
 puts "#{num1} ** #{num2} = #{num1 ** num2}"
