60adffb66c9de61d3deb00e4482f0cb0cc903b4e
Andy Holmes <andyavd01@gmail.com>
2018-08-09 12:48:11 -0400

    initial

---
+++ exercises/101-109_small_problems/easy_3/square_argument.rb
@@ -0,0 +1,21 @@
+# Squaring an argument
+
+# Using the multiply method from "Multiplying Two Numbers" problem, write
+# a method that computes the square of its argument ( the square is the 
+# result of multiplying a number by itself).
+
+# Further exploration
+# What if we wanted to generalize this method t a "power to the n" type
+# method: cubed, to the 4th power, to the 5th, etc. How would we go about
+# doing so while still using the multiply method?
+
+def multiply(num, num)
+  num1 * num2
+end
+
+def square(num, power)
+  multiply(num, num))
+end
+
+puts square(2, 2)
+puts square(-8, 2)
