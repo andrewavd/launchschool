6bfa71357de6ca3b54689b57e173fb9a34f190e5
Andy Holmes <andyavd01@gmail.com>
2018-09-24 10:51:02 -0400

    initial

---
+++ exercises/101-109_small_problems/easy_7/staggered_caps1.rb
@@ -0,0 +1,12 @@
+# staggered_caps1.rb
+# Write a method that takes a String as an argument, and returns a new String that contains the original value using a staggered
+# capitalization scheme in which every other character is caoitalized, and the remaining characters are lowercase. Characters that are
+# not letters should not be changed, but count as characters when switching between uooer and lowercase.
+
+def staggered_case(str)
+  str.chars.map.with_index { |char, i| i.odd? ? char = char.downcase : char = char.upcase }.join
+end
+
+p staggered_case('I Love Launch School!')     # == 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('ALL_CAPS')                  # == 'AlL_CaPs'
+p staggered_case('ignore 77 the 444 numbers') # == 'IgNoRe 77 ThE 444 NuMbErS'
