943e539f12c8f9fdeec767bdf2f87deeb4eae1f0
Andy Holmes <andyavd01@gmail.com>
2018-09-24 14:54:11 -0400

    initial

---
+++ exercises/101-109_small_problems/easy_7/multiply_lists.rb
@@ -0,0 +1,11 @@
+# multiply_lists.rb
+# Write a method that takes two Array arguments in which each Array contains a list of numbers, and returns a new Array that contains
+# the product of each pair of numbers from the arguments that have the same index. You may assume that the arguments contain the
+# same number of elements.
+
+def multiply_list(arr1, arr2)
+  result = arr1.map.with_index { |e, i| e * arr2[i] }
+end
+
+p multiply_list([3, 5, 7], [9, 10, 11]) # == [27, 50, 77]
+
