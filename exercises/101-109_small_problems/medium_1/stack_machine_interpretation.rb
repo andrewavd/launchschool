79663c33cc2b37b80ee525cd9a8e70f1e09a19d3
Andy Holmes <andyavd01@gmail.com>
2018-09-30 10:50:06 -0400

    full solution

--- exercises/101-109_small_problems/medium_1/stack_machine_interpretation.rb
+++ exercises/101-109_small_problems/medium_1/stack_machine_interpretation.rb
@@ -18,12 +18,63 @@
 # > (PUSH) Push the register value on to the stack, Leave the value in the register.
 # > (ADD) Pops a value from the stack and adds it to the register value, storing the result in the register.
 # > (SUB) Pops a value from the stack and subtracts it from the register value, storing the result in the register.
-# > (MULT) Pops a value from the stack and multiplies it by the tegister value, storing the result in the register.
+# > (MULT) Pops a value from the stack and multiplies it by the register value, storing the result in the register.
 # > (DIV) Pops a value from the stack and divides it into the register value, storing the integer result in the register.
-# > (MOD) Pops a valued from the stack and divides it into the register value, storing the integer remainder of the division in the register.
+# > (MOD) Pops a value from the stack and divides it into the register value, storing the integer remainder of the division in the register.
 # > (POP) Remove the topmost item from the stack and place in register.
 # > (PRINT) Print the register value.
 
-def minilang(str)
-    puts str
+# All operations are integer operations (which is only important with DIV and MOD).
+# Programs will be supplied to your language method via a string passed in as an argument. Your program may assume tat all programs are correct
+# programs; that is, they won't do anything like try to pop a non-existent value from the stack and they won't contain unknown tokens.
+# You should initialize the register to 0.
+
+def minilang(program_str)
+  register = [0]
+  stack = []
+  program_str.split.each do |instruct|
+    if instruct.to_i.to_s == instruct
+      register.push(instruct.to_i)
+    else
+      case instruct
+      when 'PUSH'
+        stack.push(register[register.size - 1])
+      when 'ADD'
+        register.push(register[-1] + stack.pop)
+      when 'SUB'
+        register.push(register[-1] - stack.pop)
+      when 'MULT'
+        register.push(register[-1] * stack.pop)
+      when 'DIV'
+        register.push(register[-1] / stack.pop)
+      when 'MOD'
+        register.push(register[-1] % stack.pop)
+      when 'POP'
+        register.push(stack.pop)
+      when 'PRINT'
+        puts register[-1]
+      end
+    end
+  end
 end
+
+minilang('PRINT') # == 0
+puts '-----'
+minilang('5 PUSH 3 MULT PRINT') # == 15
+puts '-----'
+minilang('5 PRINT PUSH 3 PRINT ADD PRINT') # == 5 3 8
+puts '-----'
+minilang('5 PUSH POP PRINT') # 5
+puts '-----'
+minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT') # 5 10 4 7
+puts '-----'
+minilang('3 PUSH PUSH 7 DIV MULT PRINT ') # 6
+puts '-----'
+minilang('4 PUSH PUSH 7 MOD MULT PRINT ') # 12
+puts '-----'
+minilang('-3 PUSH 5 SUB PRINT') # 8
+puts '-----'
+# Further exploration: (3 + (4 * 5) - 7) / (5 % 3)
+# proceed 'backwards' through equation: 5 mod 3, 3 - 7, 4 * 5, 20 + (-4), 16 / 2
+minilang('3 PUSH 5 MOD PUSH 7 PUSH 3 SUB PUSH 5 PUSH 4 MULT ADD DIV PRINT') # == 8
+minilang('6 PUSH') # nothing printed, no 'PRINT' command
