3e626cbb2eb780a72f98eb9709c1cdaaed37895a
Andy Holmes <andyavd01@gmail.com>
2018-10-05 14:01:22 -0400

    refactor iterations

--- exercises/101-109_small_problems/advanced_1/transpose_mxn.rb
+++ exercises/101-109_small_problems/advanced_1/transpose_mxn.rb
@@ -8,12 +8,15 @@
 # least 1 row and 1 column.
 
 def transpose(matrix)
-  matrix.map.with_index { |arr, idx| arr.map.with_index { |_, i| matrix[i][idx] } }
+  result = []
+  matrix[0].size.times { result << [] }
+  matrix.each { |arr| arr.each_with_index { |e, i| result[i] << e } }
+  result
 end
 
 p transpose([[1, 5, 8], [4, 7, 2], [3, 9, 6]]) # == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
 p transpose([[1, 2, 3, 4]])                    # == [[1], [2], [3], [4]]
-#transpose([[1], [2], [3], [4]])              # == [[1, 2, 3, 4]]
-#transpose([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]])
+p transpose([[1], [2], [3], [4]])              # == [[1, 2, 3, 4]]
+p transpose([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]])
 # == [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
-#transpose([[1]])                             # == [[1]]
+p transpose([[1]])                             # == [[1]]
