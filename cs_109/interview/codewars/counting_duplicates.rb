21e6a264929557401c9b5e95cb9f4842a4e550c1
Andy Holmes <andyavd01@gmail.com>
2018-11-06 11:08:41 -0500

    add problem & count

--- cs_109/interview/codewars/counting_duplicates.rb
+++ cs_109/interview/codewars/counting_duplicates.rb
@@ -1,8 +1,11 @@
 # duplicate_count.rb
 # November 5, 2018
+# 6 kyu
 
 # Problem:
-#
+# Write a method that will return the count of distinct case-insensitive alphabetic characters
+# and numeric digits that occut more than once in the input string. The input string can be assumed
+# to contain only alphabetic characters (both uppercase and lowercase) and numeric digits.
 
 def duplicate_count(text)
   text = text.downcase
@@ -19,4 +22,14 @@ def duplicate_count(text)
   result
 end
 
-p duplicate_count('aabbcddde')
+p duplicate_count('abcde') == 0
+p duplicate_count('aabbcde') == 2
+p duplicate_count('aabBcde') == 2
+p duplicate_count('indivisibility') == 1
+p duplicate_count('Indivisibilities') == 2
+p duplicate_count('aA11') == 2
+p duplicate_count('ABBA') == 2
+
+# Using Array#count
+# arr = text.downcase.chars
+# arr.uniq.count { |n| arr.count(n) > 1 }
