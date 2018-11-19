ade27c9cd389a777f4c693dbfa4d7aa55005f053
Andy Holmes <andyavd01@gmail.com>
2018-11-19 10:05:28 -0500

    initial

---
+++ exercises/120_oop_problems/classes_objects_2/id_yourself_1.rb
@@ -0,0 +1,17 @@
+# id_yourself_1.rb
+# November 19, 2018
+
+class Cat
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def identify
+    self
+  end
+end
+
+kitty = Cat.new('Sophie')
+p kitty.identify
