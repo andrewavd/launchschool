50a997fe5e2de78f8f48bc2b50c1862831725a3a
Andy Holmes <andyavd01@gmail.com>
2018-11-19 10:00:37 -0500

    initial

---
+++ exercises/120_oop_problems/classes_objects_2/hello_chloe.rb
@@ -0,0 +1,19 @@
+# hello_chloe.rb
+# November 19, 2018
+
+class Cat
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def rename(name)
+    self.name = name
+  end
+end
+
+kitty = Cat.new('Sophie')
+puts kitty.name
+kitty.rename('Chloe')
+puts kitty.name
