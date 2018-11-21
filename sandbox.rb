59535dac61a13976f4e728d300f2e3fb0bf411d6
andrewavd <andyavd01@gmail.com>
2018-11-21 10:08:29 -0500

    update

--- sandbox.rb
+++ sandbox.rb
@@ -1,21 +1,14 @@
-# Write a method that takes a string as an argument and returns the character that occurs most often in the given string. If there are multiple characters with 
-# the equal highest number of occurrences, then return the one that appears first in the string. When counting characters, consider the uppercase and lowercase 
-# version to be the same.
+class Person
+  attr_reader :name
 
-def most_common_char(str)
-  count = 0
-  common_char = ''
-  str = str.downcase
-  str.each_char do |c|
-    if str.count(c) > count
-      count = str.count(c)
-      common_char = c
-    end
+  def initialize(name)
+    @name = name
   end
-  common_char
+
+  # def name
+  #   @name
+  # end
 end
 
-p most_common_char("Hello World") == "l"
-p most_common_char("Peter Piper picked a peck of pickled peppers") == "p"
-p most_common_char("Mississippi") == "i"
-p most_common_char("Happy birthday!!!") == '!'
+andy = Person.new("Andy")
+puts andy.name
