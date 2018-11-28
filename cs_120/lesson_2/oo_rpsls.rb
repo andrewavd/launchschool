fbcabf7dc1251877b1abdd40b72ee91ed9d18d3a
Andy Holmes <andyavd01@gmail.com>
2018-11-28 11:46:15 -0500

    initial

--- cs_120/lesson_2/oo_rpsls.rb
+++ cs_120/lesson_2/oo_rpsls.rb
@@ -1,11 +1,46 @@
 # oo_rpsls.rb
 # November 28, 2018
 
-VALUES = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
+require 'io/console'
 
-module Prompt
+VALUES = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
+TITLE = VALUES.join(' ')
+
+module UxUi
   def prompt(message)
-    print("=> #{message}")
+    print("\n => #{message}")
+  end
+
+  def clear_screen
+    system('clear') || system('cls')
+  end
+
+  def hr_ln
+    puts '-' * (TITLE.length + 2)
+  end
+
+  def display_game_banner
+    clear_screen
+    hr_ln
+    puts " #{TITLE}"
+    hr_ln
+  end
+
+  def game_hold
+    print "\nPress any key to continue..."
+    STDIN.getch
+    print " \r\n"
+  end
+
+  def display_player_greeting
+    clear_screen
+    display_game_banner
+    puts "\nWelcome #{human.name}! Your opponent will be #{computer.name}."
+    game_hold
+  end
+
+  def display_goodbye_message
+    puts "\nGoodbye #{human.name}... Thank you for playing #{TITLE}!"
   end
 end
 
@@ -26,7 +61,7 @@ class Move
       lizard: ['paper', 'spock'],
       spock: ['scissors', 'rock']
     }
-    win_matrix[@value.downcase.to_sym].include?(other_move.value.downcase.to_s)
+    win_matrix[value.downcase.to_sym].include?(other_move.value.downcase.to_s)
   end
 
   def to_s
@@ -48,16 +83,23 @@ end
 # ---------------------------------------------------
 
 class Human < Player
-  include Prompt
+  include UxUi
 
   def set_name
-    n = ''
+    player_name = ''
+    esc_count = 0
     loop do
       prompt("Please enter your name: ")
-      n = gets.chomp
-      break unless n.empty? || n.squeeze == ' '
+      esc_count += 1
+      player_name = gets.chomp.strip
+      if esc_count == 3
+        puts "It appears you wish to be the 'Invisible Man', so be it!"
+        player_name = "Invisible Man"
+        sleep 2
+      end
+      break unless player_name.empty? || player_name.squeeze == ' '
     end
-    self.name = n
+    self.name = player_name
   end
 
   def obtain_player_input
@@ -75,7 +117,7 @@ class Human < Player
       r: 'Rock',
       p: 'Paper',
       s: 'Scissors',
-      l: 'Lizzard',
+      l: 'Lizard',
       k: 'Spock'
     }
     throws[player_input[0].to_sym]
@@ -101,31 +143,20 @@ end
 # ---------------------------------------------------
 
 class RPSGame
-  include Prompt
-  attr_accessor :human, :computer, :current_winner
+  include UxUi
+  attr_accessor :human, :computer, :current_winner, :game_count
 
   WIN_GAME = 3
 
   def initialize
+    display_game_banner
+    @human = Human.new
     @computer = Computer.new
+    display_player_greeting
     @current_winner = ''
-  end
-
-  def display_welcome_message
-    puts "Rock, Paper, Scissors"
-  end
-
-  def display_player_greeting
-    puts "Welcome #{self.human.name} --- Your opponent will be #{computer.name}."
-  end
-
-  def display_goodbye_message
-    puts "Goodbye #{human.name}... Thank you for playing Rock, Paper, Scissors!"
-  end
-
-  def display_moves
-    puts "#{human.name}'s move: #{human.move} vs #{computer.name}'s move: " \
-    "#{computer.move}".gsub(/s's/, "s'")
+    @game_count = 0
+    @moves_count = {Rock: 0, Paper: 0, Scissors: 0, Lizard: 0, Spock: 0}
+    @moves_history = []
   end
 
   def play_hand
@@ -140,17 +171,33 @@ class RPSGame
     end
   end
 
+  def display_score
+    clear_screen
+    display_game_banner
+    puts "--- Score Board ---".center(TITLE.size)
+    puts "#{human.name} #{human.score} | #{computer.name} #{computer.score}".center(TITLE.size)
+    puts "(First to #{WIN_GAME}, wins match)".center(TITLE.size)
+    hr_ln
+    # puts
+  end
+
   def display_hand_winner
     if current_winner == 'draw'
-      puts "This hand's a draw!"
+      puts "This hand's a draw!".center(TITLE.size)
     else
-      puts "#{current_winner} wins!"
+      puts "#{current_winner.upcase} WINS!".center(TITLE.size)
     end
   end
 
-  def display_score
-    puts "--- Score Board ---"
-    puts "#{human.name} #{human.score} | #{computer.name} #{computer.score}"
+  def display_moves
+    puts "Game #{game_count}".center(TITLE.size)
+    puts
+    display_hand_winner
+    puts
+    puts "#{human.name}'s move: #{human.move}".center(TITLE.size)
+    puts "vs".center(TITLE.size)
+    puts "#{computer.name}'s move: #{computer.move}".gsub(/s's/, "s'").center(TITLE.size)
+    hr_ln
   end
 
   def won_match?
@@ -158,13 +205,13 @@ class RPSGame
   end
 
   def display_game_winner
-    puts "#{current_winner} won the game."
+    puts "\n  #{current_winner} won the game."
   end
 
   def reset_score
     human.score = 0
     computer.score = 0
-    current_winner = ''
+    self.game_count = 0
   end
 
   def end_game?
@@ -176,23 +223,29 @@ class RPSGame
     answer == 'n' || answer == 'no'
   end
 
+  def rpsls_throw
+    human.choose
+    computer.choose
+    play_hand
+  end
+
+  def display_throw_results
+    display_score
+    display_moves
+  end
+
   def play
-    display_welcome_message
-    @human = Human.new
-    display_player_greeting
     loop do
       loop do
-        human.choose
-        computer.choose
-        play_hand
-        display_moves
-        display_hand_winner
-        display_score
+        @game_count += 1
+        rpsls_throw
+        display_throw_results
         break if won_match?
       end
       display_game_winner
       break if end_game?
       reset_score
+      display_game_banner
     end
     display_goodbye_message
   end
