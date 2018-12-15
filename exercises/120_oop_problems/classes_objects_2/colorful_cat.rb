43c1fc969798e12f4273042d5d7958321f8fa48f
andrewavd <andyavd01@gmail.com>
2018-12-15 13:02:46 -0500

    update

--- exercises/120_oop_problems/classes_objects_2/colorful_cat.rb
+++ exercises/120_oop_problems/classes_objects_2/colorful_cat.rb
@@ -2,8 +2,12 @@
 # December 15, 2018
 
 class Cat
-  attr_reader :name
-  
+  # The getter method is not needed in this case because name is not being exposed
+  # outside the instance. If the getter is used it is a preferred practice to change
+  # @name in the greet method to name.
+
+  # attr_reader :name
+
   COLOR = 'tan'
 
   def initialize(name)
@@ -11,7 +15,7 @@ class Cat
   end
 
   def greet
-    puts "Hello! My name is #{name} and I'm a #{COLOR} cat!"
+    puts "Hello! My name is #{@name} and I'm a #{COLOR} cat!"
   end
 end
 
