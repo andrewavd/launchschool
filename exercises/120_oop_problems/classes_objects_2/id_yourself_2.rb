91e3f7147613f32746f2f2b8b90e6da23ad5c5d9
andrewavd <andyavd01@gmail.com>
2018-12-15 14:14:33 -0500

    initial

---
+++ exercises/120_oop_problems/classes_objects_2/id_yourself_2.rb
@@ -0,0 +1,17 @@
+# id_yourself_2.rb
+# December 15, 2018
+
+class Cat
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    "I'm #{name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+puts kitty
