1053d0adba7f749cb5d02b653cd3695cbf508675
andrewavd <andyavd01@gmail.com>
2018-12-16 14:57:33 -0500

    update

--- exercises/120_oop_problems/accessor_methods/reading_writing.rb
+++ exercises/120_oop_problems/accessor_methods/reading_writing.rb
@@ -1,8 +1,25 @@
 # reading_writing.rb
 # December 16, 2018
 
+# For demonstration, both the "built-in" and "manual" implementaions of
+# getters & setters are presented. You would NOT use both in production.
+
 class Person
-  attr_accessor :name
+  # built in
+  attr_reader :name # getter
+  attr_writer :name # setter
+
+  # manual implementations of getter & setter
+
+  # getter
+  def name
+    @name
+  end
+
+  # setter
+  def name=(name)
+    @name = name
+  end
 end
 
 person1 = Person.new
