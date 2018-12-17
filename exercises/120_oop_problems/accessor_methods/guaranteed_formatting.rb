c0c9f1869418ecc3c459c612aaa5569f2ee8a113
andrewavd <andyavd01@gmail.com>
2018-12-16 20:08:26 -0500

    initial

---
+++ exercises/120_oop_problems/accessor_methods/guaranteed_formatting.rb
@@ -0,0 +1,14 @@
+# guaranteed_formatting.rb
+# December 16, 2018
+
+class Person
+  attr_reader :name
+
+  def name=(name)
+    @name = name.capitalize
+  end
+end
+
+person1 = Person.new
+person1.name = 'eLiZaBeTh'
+puts person1.name
