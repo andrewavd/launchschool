7f4d65e41764b13ab9f09271e6566ad6f18b8617
andrewavd <andyavd01@gmail.com>
2018-12-23 07:28:53 -0500

    initial

---
+++ exercises/120_oop_problems/easy_1/fix_books2.rb
@@ -0,0 +1,16 @@
+# fix_books2.rb
+# December 23, 2018
+
+class Book
+  attr_accessor :author, :title
+
+  def to_s
+    %("#{title}", by #{author})
+  end
+end
+
+book = Book.new
+book.author = "Neil Stephenson"
+book.title = "Snow Crash"
+puts %(The author of "#{book.title}" is #{book.author}.)
+puts %(book = #{book}.)
