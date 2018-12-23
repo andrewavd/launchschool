4f7127bca849a90812180f0b3f143f3a3dac4d7c
andrewavd <andyavd01@gmail.com>
2018-12-23 10:35:51 -0500

    initial

---
+++ exercises/120_oop_problems/easy_1/car_mileage.rb
@@ -0,0 +1,24 @@
+# car_mileage.rb
+# December 23, 2018
+
+class Car
+  attr_accessor :mileage
+
+  def initialize
+    @mileage = 0
+  end
+
+  def increment_mileage(miles)
+    total = mileage + miles
+    self.mileage = total
+  end
+
+  def print_mileage
+    puts mileage
+  end
+end
+
+car = Car.new
+car.mileage = 5000
+car.increment_mileage(678)
+car.print_mileage # ==> 5678
